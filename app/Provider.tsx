b3d261ebeac3fd4576fc5b5cfdbceb6ee92a8d56
Divyansh Mahavar <79765685+divyanshkm@users.noreply.github.com>
2024-12-24 19:12:29 +0530

    1:00:45 Mark

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,8 @@
+"use client";
+
 import React from "react";
 import {
   LiveblocksProvider,
-  RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/Loader";
